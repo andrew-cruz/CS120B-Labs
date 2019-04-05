3e8e70d80fbd5354590f6b77487b778f8b439bae
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-05 14:58:11 -0700

    Part 4

--- lab02/acruz029_jkane002_lab2_part4/acruz029_jkane002_lab2_part4/main.c
+++ lab02/acruz029_jkane002_lab2_part4/acruz029_jkane002_lab2_part4/main.c
@@ -6,55 +6,32 @@
  */
 #include <avr/io.h>
 
-unsigned char GetBit(char port, char bit_pos) {
-	return ( port & (0x01 << bit_pos) );
-}
-
-unsigned char SetBit(char  pin, char bit_pos, char bit_val) {
-	return (bit_val ? pin | (0x01 << bit_pos) : pin & ~(0x01 << bit_pos));
-}
 
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
-	DDRC = 0x00; PORTC = 0xFF;
-	
-	DDRD = 0xFF; PORTD = 0x00;
+	// Init output
+	DDRD = 0x00; PORTD = 0x00;
 	
 	unsigned char actual_weight = 0x00;
-	unsigned char shifted_weight = 0x00; 
+	unsigned char edge_weight = 0x00;s
 	
-	unsigned char tempA;
-	unsigned char tempB;
-	unsigned char tempC;
-	unsigned char tempD;
-	
-    /* Replace with your application code */
-    while (1) 
-    {
-		tempA = PINA;
-		tempB = PINB;
-		tempC = PINC;
+	while(1){
+		//Get total weight
+		actual_weight = PINA + PINB + PINC;
+		//Get edge weight difference
+		edge_weight = PINA > PINC ? PINA - PINC : PINC - PINA;
 		
-		if(tempA >= 128) {
-			tempA = tempA >> 1;
+		//Check to see if weight is exceeded
+		if (actual_weight > 140) {
+			PORTD = PORTD | 0x01;
 		}
-		if(tempB >= 128) {
-			tempB = tempB >> 1;
-		}
-		if(tempC >= 128) {
-			tempC = tempC >> 1;
+		//Check to see if edges are balanced
+		if (edge_weight > 80) {
+			PORTD = PORTD | 0x02;
 		}
 		
-		actual_weight = tempA + tempB + tempC;
-		
-		if(actual_weight >= 140) {
-			PORTD = SetBit(PORTD, 0, 1);
-		} else {
-			PORTD = 0x00;
-		}
-		
+		//Store actual weight to PORTD 
+		PORTD = PORTD | actual_weight;
 	}
 }
 
