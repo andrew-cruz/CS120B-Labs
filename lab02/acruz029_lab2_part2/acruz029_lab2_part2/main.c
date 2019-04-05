176fcd3e9c8f498951a78e5445f44f1005e37641
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-04 20:12:08 -0700

    Part 2

--- lab02/acruz029_lab2_part2/acruz029_lab2_part2/main.c
+++ lab02/acruz029_lab2_part2/acruz029_lab2_part2/main.c
@@ -1,18 +1,34 @@
 /*
- * acruz029_lab2_part2.c
- *
- * Created: 4/4/2019 10:39:17 AM
- * Author : ucrcse
- */ 
-
+ * Andrew Cruz acruz029@ucr.edu
+ * Jonathan Kaneshiro jkane002@ucr.edu
+ * Lab Section: 24
+ * Assignment: Lab #2 Exercise #2
+ */
 #include <avr/io.h>
 
+unsigned char GetBit(char port, char bit_pos) {
+	return ( port & (0x01 << bit_pos) );
+}
 
 int main(void)
 {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char tmp;
+	//unsigned char cntavail = 0x00;
+	
     /* Replace with your application code */
     while (1) 
     {
+		unsigned char cntavail = 0x00;
+		tmp = PINA;
+		cntavail = GetBit(tmp, 0) == 0x00 ? cntavail + 0x01 : cntavail;
+		cntavail = GetBit(tmp, 1) == 0x00 ? cntavail + 0x01 : cntavail;
+		cntavail = GetBit(tmp, 2) == 0x00 ? cntavail + 0x01 : cntavail;
+		cntavail = GetBit(tmp, 3) == 0x00 ? cntavail + 0x01 : cntavail;
+		
+		PORTC = cntavail;
     }
 }
 
