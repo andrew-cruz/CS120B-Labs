212fd116be18c13a54a553ea82bfe78bab7dda1f
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-10 13:34:58 -0700

    Changed header

--- lab03/acruz029_jkane002_lab03_part1/acruz029_jkane002_lab03_part1/main.c
+++ lab03/acruz029_jkane002_lab03_part1/acruz029_jkane002_lab03_part1/main.c
@@ -1,9 +1,14 @@
-/*
- * acruz029_jkane002_lab03_part1.c
- *
- * Created: 4/9/2019 10:29:37 AM
- * Author : ucrcse
- */ 
+/*	Partner 1 Name & E-mail: Andrew Cruz acruz029@ucr.edu
+ *	Partner 2 Name & E-mail: Jonathan Kaneshiro jkane002@ucr.edu
+ *	Lab Section: 24
+ *	Assignment: Lab # 3  Exercise # 1 
+ *	Exercise Description: [optional - include for your own benefit]
+ *		Count the number of 1s on ports A and B and output that number on port C.
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
 
 #include <avr/io.h>
 
@@ -13,22 +18,30 @@ unsigned char getBit(unsigned char x, unsigned char k) {
 
 int main(void)
 {
+		//init inputs
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0x00; PORTB = 0xFF;
+		//init outputs
 	DDRC = 0xFF; PORTC = 0x00;
-    /* Replace with your application code */
     
-	
+		//Temp variables for read
+	unsigned char tempA = 0x00;
+	unsigned char tempB = 0x00;
 	while (1) 
     {
+			//keep count
 		unsigned char count = 0x00;
+		tempA = PINA;
+		tempB = PINB;
+		
+			//check number of 1's in PINA and PINB
 		for(int i = 0; i < 8; i++) {
-			if(getBit(PINA,i) != 0)
+			if(getBit(tempA,i) != 0)
 				count++;
-			if(getBit(PINB, i) != 0)
+			if(getBit(tempB, i) != 0)
 				count++;
 		}
-		
+			//Set to PORTC
 		PORTC = count;
     }
 }
