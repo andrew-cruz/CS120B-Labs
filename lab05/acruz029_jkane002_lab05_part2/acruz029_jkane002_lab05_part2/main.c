77d8ac7844ecba6f4d9eba8666b953a133651288
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-19 13:30:39 -0700

    lab05

--- lab05/acruz029_jkane002_lab05_part2/acruz029_jkane002_lab05_part2/main.c
+++ lab05/acruz029_jkane002_lab05_part2/acruz029_jkane002_lab05_part2/main.c
@@ -18,179 +18,57 @@
  
 #include <avr/io.h>
 
-enum Num_States {Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine} Num_State;
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+
+
+
+enum Num_States {Wait, Increment, Decrement, ButtonRelease, Reset, ResetRealse} Num_State;
 
 void State_Machine() {
 	unsigned char PA_01 = PINA & 0x03;
+	PA_01 = ~PA_01 & 0x03;
 	
 	switch(Num_State) {
-		case Zero: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Zero; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = One;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Zero;
+		case Wait: {
+			if(PA_01 == 0x01) { //Increment PA1 = 0 PA0 = 1
+				Num_State = Increment;
+			} else if(PA_01 == 0x02) { //Decrement PA1 = 1 PA0 = 0
+				Num_State = Decrement;
+			} else if(PA_01 == 0x03) { //Reset PA1 = 1 PA0 = 1
+				Num_State = Reset;
+			} else { //Anything else resets 
+				Num_State = Reset;
 			}
-			break;
 		}
-		case One: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Zero; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Two;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = One;
+		case Increment: {
+			Num_State = Wait;
+		}
+		case Decrement: {
+			Num_State = Wait;
+		}
+		case Reset: {
+			if(PA_01 == 0x03) { //Both Button still depressed
+				Num_State = Reset;
+			} else if (PA_01 == 0x00) { //If both buttons get depressed
+				Num_State = Wait;
+			} else { //Anything else goes to reset
+				Num_State = Reset;
 			}
-			break;
 		}
-		case Two: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = One; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Three;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Two;
-			}
-			break;
-		}
-		case Three: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Two; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Four;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Three;
-			}
-			break;
-		}
-		case Four: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Three; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Five;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Four;
-			}
-			break;
-		}
-		case Five: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Four; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Six;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Five;
-			}
-			break;
-		}
-		case Six: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Five; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Seven;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Six;
-			}
-			break;
-		}
-		case Seven: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Six; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Eight;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Seven;
-			}
-			break;
-		}
-		case Eight: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Seven; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Nine;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Eight;
-			}
-			break;
-		}
-		case Nine: {
-			if(PA_01 == 0x02) { //Decrementing: PA0 is 0 PA1 is 1
-				Num_State = Eight; //Stay In Same State
-				} else if(PA_01 == 0x01) { //Incrementing: PA0 is 1 PA1 is 0
-				Num_State = Nine;
-				} else if(PA_01 == 0x03) { //Both Depressed: PA0 is 1 PA1 is 1
-				Num_State = Zero;
-				} else { //Both Not Pressed
-				Num_State = Nine;
-			}
-			break;
-		}
-		default : { //Errors go to state Zero
-			Num_State = Zero;
+		default : { //Errors go to state Wait
+			Num_State = Reset;
 			break;
 		}
 	}
-	unsigned char tempB = 0xFF;
+	unsigned char tempB = 0x00;
+	
 	switch(Num_State) {
-		case Zero: {
-			tempB = 0;
-			break;
-		}
-		case One: {
-			tempB = 1;
-			break;
-		}
-		case Two: {
-			tempB = 2;
-			break;
-		}
-		case Three: {
-			tempB = 3;
-			break;
-		}
-		case Four: {
-			tempB = 4;
-			break;
-		}
-		case Five: {
-			tempB = 5;
-			break;
-		}
-		case Six: {
-			tempB = 6;
-			break;
-		}
-		case Seven: {
-			tempB = 7;
-			break;
-		}
-		case Eight: {
-			tempB = 8;
-			break;
-		}
-		case Nine: {
-			tempB = 9;
-			break;
-		}
 		default : { //Errors go to state Zero
 			tempB = 0xFF;
 			break;
@@ -209,6 +87,7 @@ int main(void)
 	
 	while (1)
 	{
+		//PORTB = PINA;
 		State_Machine();
 		
 	}
