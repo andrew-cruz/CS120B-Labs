f24ab214e31f96d3b4a3138deaafa5da77f344fa
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-19 14:27:37 -0700

    Jon

--- lab05/acruz029_jkane002_lab05_part2/acruz029_jkane002_lab05_part2/main.c
+++ lab05/acruz029_jkane002_lab05_part2/acruz029_jkane002_lab05_part2/main.c
@@ -28,31 +28,55 @@ unsigned char GetBit(unsigned char x, unsigned char k) {
 
 
 
-enum Num_States {Wait, Increment, Decrement, ButtonRelease, Reset, ResetRealse} Num_State;
+enum Num_States {Wait, Increment, Decrement, Reset} Num_State;
 
 void State_Machine() {
 	unsigned char PA_01 = PINA & 0x03;
 	PA_01 = ~PA_01 & 0x03;
+	unsigned char tempB = PORTB;
 	
 	switch(Num_State) {
 		case Wait: {
 			if(PA_01 == 0x01) { //Increment PA1 = 0 PA0 = 1
 				Num_State = Increment;
+				tempB = (tempB >= 0x09) ? 0x09 : tempB + 0x01;
 			} else if(PA_01 == 0x02) { //Decrement PA1 = 1 PA0 = 0
 				Num_State = Decrement;
+				tempB = (tempB <= 0x00) ? 0x00 : tempB - 0x01;
 			} else if(PA_01 == 0x03) { //Reset PA1 = 1 PA0 = 1
 				Num_State = Reset;
-			} else { //Anything else resets 
-				Num_State = Reset;
+				tempB = 0x00;
+			} else { //Anything else just wait 
+				Num_State = Wait;
 			}
+			break;
 		}
 		case Increment: {
-			Num_State = Wait;
+			if(PA_01 == 0x01) { //If button is still down stay in state
+				Num_State = Increment;
+			} else if(PA_01 == 0x00) { //If both button are released go waiting state
+				Num_State = Wait;
+			} else if(PA_01 == 0x03) { //If both buttons are down go to reset
+				Num_State = Reset;
+			} else { //Anything else 
+				Num_State = Wait;
+			}
+			break;
 		}
 		case Decrement: {
-			Num_State = Wait;
+			if(PA_01 == 0x02) { //If still prseed down wait in this state
+				Num_State = Decrement; 
+			} else if(PA_01 == 0x00) { //If both buttons released then go to wait state
+				Num_State = Wait;
+			} else if(PA_01 == 0x03) { //If both buttons pressed go to reset
+				Num_State = Reset;
+			} else { //Else
+				Num_State = Wait;
+			}
+			break;
 		}
 		case Reset: {
+			tempB = 0x00;
 			if(PA_01 == 0x03) { //Both Button still depressed
 				Num_State = Reset;
 			} else if (PA_01 == 0x00) { //If both buttons get depressed
@@ -60,21 +84,13 @@ void State_Machine() {
 			} else { //Anything else goes to reset
 				Num_State = Reset;
 			}
+			break;
 		}
 		default : { //Errors go to state Wait
 			Num_State = Reset;
 			break;
 		}
 	}
-	unsigned char tempB = 0x00;
-	
-	switch(Num_State) {
-		default : { //Errors go to state Zero
-			tempB = 0xFF;
-			break;
-		}
-	}
-	
 	PORTB = tempB;
 }
 
@@ -82,12 +98,11 @@ void State_Machine() {
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	Num_State = Seven;
+	DDRB = 0xFF; PORTB = 0x07;
+	Num_State = Wait;
 	
 	while (1)
 	{
-		//PORTB = PINA;
 		State_Machine();
 		
 	}
