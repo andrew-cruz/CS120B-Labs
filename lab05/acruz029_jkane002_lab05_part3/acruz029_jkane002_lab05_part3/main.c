e3ff4723306ad4b4c776d62f698d7e6a7d09f52e
Andrew Cruz <andrewcruz1000@gmail.com>
2019-04-19 15:02:39 -0700

    part 3

--- lab05/acruz029_jkane002_lab05_part3/acruz029_jkane002_lab05_part3/main.c
+++ lab05/acruz029_jkane002_lab05_part3/acruz029_jkane002_lab05_part3/main.c
@@ -12,13 +12,71 @@
  */
  
 #include <avr/io.h>
+#define LEFT 0
+#define RIGHT 1
 
+enum ButtonPress{Init, Wait, ButtonDown}ButtonPressed;
+
+unsigned char DIRECTION;
+
+void SM_Sequence() {
+	unsigned char A_0 = PINA & 0x01;
+	A_0 = ~A_0 & 0x01; //Gets button press inverted
+	
+	unsigned char tempB = PORTB;
+
+	switch (ButtonPressed) {
+		case Init: { //Starting state
+			ButtonPressed = Wait;
+			tempB = 0x00;
+			DIRECTION = LEFT;
+			break;
+		}
+		case Wait: { //Waiting for button to be pressed
+			if(A_0) { //Button is pressed
+				ButtonPressed = ButtonDown;		//Move to next state
+				
+				if(tempB >= 0x7F) {	//Check if we need to go in other direction
+					DIRECTION = RIGHT;
+				} else if(tempB <= 0x00) { //Check to see if we need to go in other direction
+					DIRECTION = LEFT;
+				}
+				
+				if(DIRECTION == RIGHT) {	//Modify B according to direction
+					tempB = tempB >> 1;
+				} else if(DIRECTION == LEFT) {
+					tempB = (tempB << 1) | 0x01;
+				}
+				
+				PORTB = tempB;
+			} else {	//Anything else go to wait state
+				ButtonPressed = Wait;
+			}
+			break;
+		}
+		case ButtonDown: {	//Button is pressed state
+			if(A_0) {	//Button is being held
+				ButtonPressed = ButtonDown;
+			} else {	//Button is released
+				ButtonPressed = Wait;
+			}
+			break;
+		}
+		default: { //Errors go to init
+			ButtonPressed = Init;
+			break;
+		}
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	ButtonPressed = Init;
     while (1) 
     {
+		SM_Sequence();
     }
 }
 
